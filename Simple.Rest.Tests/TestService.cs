7150be7c390c1717de9d213ab863e2f0dcdd7eb4
Ollie Riches <ollie.riches@btinternet.com>
2012-10-17 19:46:37 +0100

    Tests now support continuous testing frameworks

--- Simple.Rest.Tests/TestService.cs
+++ Simple.Rest.Tests/TestService.cs
@@ -7,57 +7,58 @@ namespace Simple.Rest.Tests
     using Controllers;
     using Dto;
 
-    public class TestService : IDisposable
+public class TestService : IDisposable
+{
+    private readonly string _baseUrl;
+
+    private HttpSelfHostServer _server;
+
+    public IList<Employee> Employees;
+    public IList<Report> Reports;
+    public TimeSpan Delay;
+
+    public TestService(string baseUrl)
     {
-        private readonly string _baseUrl;
+        _baseUrl = baseUrl;
 
-        private HttpSelfHostServer _server;
-
-        public IList<Employee> Employees;
-        public IList<Report> Reports;
-        public TimeSpan Delay;
-
-        public TestService(string baseUrl)
-        {
-            _baseUrl = baseUrl;
-
-            SetUpControllers();
-            SetUpHost();
-        }
-
-        public void Dispose()
-        {
-            _server.Dispose();
-        }
-
-        private void SetUpHost()
-        {
-            var config = new HttpSelfHostConfiguration(_baseUrl);
-
-            config.Routes.MapHttpRoute("DefaultAPI", "api/{controller}/{id}", new { id = RouteParameter.Optional });
-
-            _server = new HttpSelfHostServer(config);
-            _server.OpenAsync().Wait();
-        }
-
-        private void SetUpControllers()
-        {
-            Employees = new List<Employee>
-            {  
-                new Employee { Id = 1, FirstName = "Ollie", LastName = "Riches" },
-                new Employee { Id = 2, FirstName = "Steve", LastName = "Austin" },
-            };
-
-            EmployeesController.Employees = Employees;
-
-            Delay = TimeSpan.FromSeconds(3);
-            Reports = new List<Report>
-            {  
-                new Report { Id = 1, Name = "SlowReport" }
-            };
-
-            ReportsController.Delay = Delay;
-            ReportsController.Reports = Reports;
-        }
+        SetUpControllers();
+        SetUpHost();
     }
+
+    public void Dispose()
+    {
+        _server.CloseAsync().Wait();
+        _server.Dispose();
+    }
+
+    private void SetUpHost()
+    {
+        var config = new HttpSelfHostConfiguration(_baseUrl);
+
+        config.Routes.MapHttpRoute("DefaultAPI", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+
+        _server = new HttpSelfHostServer(config);
+        _server.OpenAsync().Wait();
+    }
+
+    private void SetUpControllers()
+    {
+        Employees = new List<Employee>
+        {  
+            new Employee { Id = 1, FirstName = "Ollie", LastName = "Riches" },
+            new Employee { Id = 2, FirstName = "Steve", LastName = "Austin" },
+        };
+
+        EmployeesController.Employees = Employees;
+
+        Delay = TimeSpan.FromSeconds(3);
+        Reports = new List<Report>
+        {  
+            new Report { Id = 1, Name = "SlowReport" }
+        };
+
+        ReportsController.Delay = Delay;
+        ReportsController.Reports = Reports;
+    }
+}
 }
