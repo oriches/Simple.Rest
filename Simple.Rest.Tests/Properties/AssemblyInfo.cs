f818adb5ed04b59403d76802ffe023e34106cb8e
Ollie Riches <ollie.riches@btinternet.com>
2015-10-24 18:23:24 +0100

    Updated nuget version and published to nuget

--- Simple.Rest.Tests/Properties/AssemblyInfo.cs
+++ Simple.Rest.Tests/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Simple.Rest.Tests")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Simple.Rest/Properties/AssemblyInfo.cs
+++ Simple.Rest/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("Simple.Rest")]
 [assembly: AssemblyDescription("Simple Rest Client-side Library")]
 [assembly: AssemblyProduct("Simple.Rest")]
-[assembly: AssemblyCopyright("Simple.Rest Copyright © 2014")]
+[assembly: AssemblyCopyright("Simple.Rest Copyright © 2015")]
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: AssemblyVersion("0.20.0.0")]
